be2f20badff7d3dfa767f448fd5bc4bab9de4300
Renia <snrenia@gmail.com>
2026-09-22 11:11:04 +0200

    Added 2 more test to resourceManagerTest

--- automated-tests/unity-edit-mode/ResourceManagerTests/ResourceManagerTests.cs
+++ automated-tests/unity-edit-mode/ResourceManagerTests/ResourceManagerTests.cs
@@ -19,4 +19,44 @@ public class ResourceManagerTests
         // Cleanup
         Object.DestroyImmediate(gameObject);
     }
+
+    [Test]
+public void SpendWood_WithEnoughWood_DecreasesWoodAmount()
+{
+    // Arrange
+    GameObject gameObject = new GameObject();
+    ResourceManager resourceManager = gameObject.AddComponent<ResourceManager>();
+
+    resourceManager.wood = 20;
+
+    // Act
+    bool result = resourceManager.SpendWood(5);
+
+    // Assert
+    Assert.IsTrue(result);
+    Assert.AreEqual(15, resourceManager.wood);
+
+    // Cleanup
+    Object.DestroyImmediate(gameObject);
+}
+
+[Test]
+public void SpendWood_WithoutEnoughWood_DoesNotChangeWoodAmount()
+{
+    // Arrange
+    GameObject gameObject = new GameObject();
+    ResourceManager resourceManager = gameObject.AddComponent<ResourceManager>();
+
+    resourceManager.wood = 3;
+
+    // Act
+    bool result = resourceManager.SpendWood(5);
+
+    // Assert
+    Assert.IsFalse(result);
+    Assert.AreEqual(3, resourceManager.wood);
+
+    // Cleanup
+    Object.DestroyImmediate(gameObject);
+}
 }
